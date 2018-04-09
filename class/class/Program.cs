dcb6d31e72db3a415e0756cb3326245eba5c2920
donggyu <34573243+donggyushin@users.noreply.github.com>
2018-04-09 15:59:05 +0900

    Class and windowForm
    Class and windowForm

--- class/class/Program.cs
+++ class/class/Program.cs
@@ -8,8 +8,35 @@ namespace @class
 {
     class Program
     {
+
+        class MyMath
+        {
+            public static double PI = 3.14;
+        }
+
         static void Main(string[] args)
         {
+            List<int> intList = new List<int>() { 1,2,3,4,5,6};
+            foreach (var item in intList)
+            {
+                Console.WriteLine(intList.Count + " : " + item);
+            }
+
+            Console.WriteLine(MyMath.PI);
+
+
+            List<int> list = new List<int>();
+            list.Add(1);
+            list.Add(2);
+            list.Add(3);
+            list.Add(4);
+            list.Add(5);
+
+            foreach ( var item in list)
+            {
+                Console.WriteLine(item);
+            }
+
         }
     }
 }
