15c666bac00049351508b0ce8999ea2ec2ed857e
donggyu <34573243+donggyushin@users.noreply.github.com>
2018-04-09 16:02:17 +0900

    description in Korean for Math class
    description in Korean for Math class

--- MathBasic/Program.cs
+++ MathBasic/Program.cs
@@ -10,12 +10,12 @@ namespace MathBasic
     {
         static void Main(string[] args)
         {
-            Console.WriteLine(Math.Abs(-52273));
-            Console.WriteLine(Math.Ceiling(52.273));
-            Console.WriteLine(Math.Floor(52.273));
-            Console.WriteLine(Math.Max(52, 273));
-            Console.WriteLine(Math.Min(52, 273));
-            Console.WriteLine(Math.Round(52.273));
+            Console.WriteLine(Math.Abs(-52273));    //절대값
+            Console.WriteLine(Math.Ceiling(52.273));    //올림
+            Console.WriteLine(Math.Floor(52.273));  //내림
+            Console.WriteLine(Math.Max(52, 273));   //최대값
+            Console.WriteLine(Math.Min(52, 273));   //최솟값
+            Console.WriteLine(Math.Round(52.273));  //반올림
         }
     }
 }
